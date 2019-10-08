d95d3abde202770737f434ab319dd490dda349fe
G-Captain <geerex@outlook.com>
2019-10-08 22:07:43 +0200

    Updates

---
+++ precache-manifest.fcfcec21aba7b845ffa9701f74ac3e72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3d4dfb63eb1b2641b47a55d2c032700",
+    "url": "/periodic-table/index.html"
+  },
+  {
+    "revision": "3913d808508ffb2c3650",
+    "url": "/periodic-table/static/css/main.22994d9f.chunk.css"
+  },
+  {
+    "revision": "0d4fd189da4f3a54bf38",
+    "url": "/periodic-table/static/js/2.2eaabe8d.chunk.js"
+  },
+  {
+    "revision": "3913d808508ffb2c3650",
+    "url": "/periodic-table/static/js/main.3d1298ec.chunk.js"
+  },
+  {
+    "revision": "549068d415007918ee61",
+    "url": "/periodic-table/static/js/runtime-main.0645f97b.js"
+  }
+]);
